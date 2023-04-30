e0e5e6ab9b4f849283b1493b327ab737fa48191f
charity <charity.joseph@students.moringaschool.com>
2023-04-30 15:15:18 +0300

    completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [Questions, setQuestions] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((data)=> {
+      setQuestions(data)
+    });
+
+  }, []);
+  
+  function handleDeleteItem(id) {
+  fetch(`http://localhost:4000/questions/${id}`, {
+    method: 'DELETE'
+  })
+    .then((r) => r.json ())
+    .then(()=> {
+      const updatedQuestions = Questions.filter(
+        (Question) => Question.id !== id);
+        setQuestions(updatedQuestions);
+
+    })
+  }
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        <QuestionItem handleDeleteItem = {handleDeleteItem}/>
+      </ul>
     </section>
   );
 }
